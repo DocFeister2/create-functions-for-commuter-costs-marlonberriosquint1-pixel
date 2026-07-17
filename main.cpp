bbba75b2ec3ab7754c0f18f0f2c7ae44365bf7de
marlonberriosquint1-pixel <marlon_berriosquint1@my.vcccd.edu>
2026-07-17 01:45:12 +0000

    b

--- main.cpp
+++ main.cpp
@@ -20,10 +20,9 @@ double toCommutes(double dallars) {
     return dallars / costPerCommute;
 }
 int main(){ 
-  cout << "$20 will buy you: " << toCommutes(20) << " commutes Miles." << endl;
-  cout << "$100.50 will buy you: " << toCommutes(100.50) << " commutes Miles." << endl;
-  cout << "$500 will buy you: " << toCommutes(500) << " commutes Miles." << endl;
-  cout << "$8000 will buy you: " << toCommutes(8000) << " commutes Miles." << endl;
+  cout << "$20 will buy you: " << toCommutes(20) << " commutes ." << endl;
+  cout << "$100.50 will buy you: " << toCommutes(100.50) << " commutes ." << endl;
+  cout << "$500 will buy you: " << toCommutes(500) << " commutes ." << endl;
 
   cout << "Enter the number of commutes you'd like to do:" << endl;
   double myCommutes;
